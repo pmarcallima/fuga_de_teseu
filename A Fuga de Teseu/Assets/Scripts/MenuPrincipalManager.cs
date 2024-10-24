fc1ec20e67f915a1a2ed159c5319450e604732ee
SamaraneGabriel <samarane.gabriel@gmail.com>
2024-10-24 13:31:57 -0300

    finalizando interface e adicionando funcionalidades

--- A Fuga de Teseu/Assets/Scripts/MenuPrincipalManager.cs
+++ A Fuga de Teseu/Assets/Scripts/MenuPrincipalManager.cs
@@ -6,6 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField]private GameObject painel_menu_inicial;
     [SerializeField]private GameObject painel_creditos;
+    [SerializeField]private GameObject painel_opcoes;
 
     public void abrir_creditos()
     {
@@ -18,5 +19,23 @@ public class NewBehaviourScript : MonoBehaviour
         painel_menu_inicial.SetActive(true);
         painel_creditos.SetActive(false);
     }
+
+    public void abrir_opcoes()
+    {
+        painel_menu_inicial.SetActive(false);
+        painel_opcoes.SetActive(true);
+    }
+
+    public void fechar_opcoes()
+    {
+        painel_menu_inicial.SetActive(true);
+        painel_opcoes.SetActive(false);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Labirinto-01");
+    }
+
 
 }
