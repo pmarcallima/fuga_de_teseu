e229e81fd8009f1428d3b41b98ea97b4cd7b7bcc
Pedro Maia <prmaia2005@gmail.com>
2024-10-24 22:00:38 -0300

    Level Change

--- A Fuga de Teseu/Assets/Scripts/CollisionTeseuEnemy.cs
+++ A Fuga de Teseu/Assets/Scripts/CollisionTeseuEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class CollisionTeseuEnemy : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class CollisionTeseuEnemy : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Enemy") {
             Destroy(gameObject);
+            SceneManager.LoadScene("Menu");
         }
     }
-    
 }
